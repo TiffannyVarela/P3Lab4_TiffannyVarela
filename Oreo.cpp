7b06b9a350435025a3e4064996d28c833650d413
TiffannyVarela <tiffanny_varela529@unitec.edu>
2019-08-09 14:26:18 -0600

    Oreo

---
+++ Oreo.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string.h>
+#include "Mejora.cpp"
+
+using namespace std;
+
+#ifndef OREO_CPP
+#define OREO_CPP
+
+class Oreo : public Mejora{
+	protected:
+		
+	public:
+		Oreo(){
+		}
+		
+		int Mejorar(){
+			return produccion=2;
+		}
+};
+
+#endif
