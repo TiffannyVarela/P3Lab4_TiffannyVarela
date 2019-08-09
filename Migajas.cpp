ba87d5b4305da44392c9e12dce4127d202b6c841
TiffannyVarela <tiffanny_varela529@unitec.edu>
2019-08-09 14:26:30 -0600

    Migajas

---
+++ Migajas.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string.h>
+#include "Mejora.cpp"
+
+using namespace std;
+
+#ifndef MIGAJAS_CPP
+#define MIGAJAS_CPP
+
+class Migajas : public Mejora{
+	protected:
+		
+	public:
+		Migajas(){
+		}
+		
+		int Mejorar(){
+			return produccion=3;
+		}
+};
+
+#endif
